782eaa072f611d9c30628b1ca597833f854d30ef
Thuy Duong <tttduong23@gmail.com>
2024-11-26 21:29:35 +0700

    gắn header

--- angular_project_web/src/app/app.component.ts
+++ angular_project_web/src/app/app.component.ts
@@ -11,5 +11,8 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'angular_project_web';
   frame4 = 'assets/images/Frame 4.png'
-
+  logo='assets/images/logo.png';
+  search ='assets/images/search-icon.png';
+  filter = 'assets/images/Filter.png';
+  avt ='assets/images/avatar.jpg';
 }
