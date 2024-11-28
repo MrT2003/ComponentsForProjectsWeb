53d05bfa8e556aad1856548d2df92a2d78f9dc9a
MrT2008 <haitrieu6212@gmail.com>
2024-11-28 22:48:57 +0700

    fix setting

--- angular_project_web/src/app/pages/setting-page/setting-page.component.ts
+++ angular_project_web/src/app/pages/setting-page/setting-page.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-setting-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './setting-page.component.html',
-  styleUrl: './setting-page.component.css'
+  styleUrl: './setting-page.component.css',
 })
 export class SettingPageComponent {
   lokiPath = 'assets/images/loki.jpg';
@@ -15,13 +16,20 @@ export class SettingPageComponent {
   logoutPath = 'assets/images/Log Out.png';
 
   // icon left menu
-show = 'assets/res-leftmenu/Arrow down-circle.png';
-home = 'assets/res-leftmenu/Squircle.png';
-sort = 'assets/res-leftmenu/Discover.png';
-recent = 'assets/res-leftmenu/Recent.png';  
-playlists = 'assets/res-leftmenu/Playlists.png';
-watchlist = 'assets/res-leftmenu/Watchlist.png';
-continue = 'assets/res-leftmenu/Continue.png';
-settings = 'assets/res-leftmenu/Settings.png';
-logout = 'assets/res-leftmenu/Log Out.png';
+  show = 'assets/res-leftmenu/Arrow down-circle.png';
+  home = 'assets/res-leftmenu/Squircle.png';
+  sort = 'assets/res-leftmenu/Discover.png';
+  recent = 'assets/res-leftmenu/Recent.png';
+  playlists = 'assets/res-leftmenu/Playlists.png';
+  watchlist = 'assets/res-leftmenu/Watchlist.png';
+  continue = 'assets/res-leftmenu/Continue.png';
+  settings = 'assets/res-leftmenu/Settings.png';
+  logout = 'assets/res-leftmenu/Log Out.png';
+  sideBarPath = 'assets/res-leftmenu/sidebar.png';
+
+  isCollapsed = false; // Trạng thái menu: mở (false) hoặc thu nhỏ (true)
+
+  toggleMenu(): void {
+    this.isCollapsed = !this.isCollapsed; // Đổi trạng thái
+  }
 }
