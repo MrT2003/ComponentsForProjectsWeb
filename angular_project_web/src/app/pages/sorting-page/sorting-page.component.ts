f0d52179d33d22d7b8f26ae6d6515e3166ed28a2
Thuy Duong <tttduong23@gmail.com>
2024-11-28 10:38:14 +0700

    add left menu to sorting page

--- angular_project_web/src/app/pages/sorting-page/sorting-page.component.ts
+++ angular_project_web/src/app/pages/sorting-page/sorting-page.component.ts
@@ -10,4 +10,15 @@ import { Component } from '@angular/core';
 export class SortingPageComponent {
   lokiPath = 'assets/images/loki.jpg';
 
+  // icon left menu
+show = 'assets/res-leftmenu/Arrow down-circle.png';
+home = 'assets/res-leftmenu/Squircle.png';
+sort = 'assets/res-leftmenu/Discover.png';
+recent = 'assets/res-leftmenu/Recent.png';  
+playlists = 'assets/res-leftmenu/Playlists.png';
+watchlist = 'assets/res-leftmenu/Watchlist.png';
+continue = 'assets/res-leftmenu/Continue.png';
+settings = 'assets/res-leftmenu/Settings.png';
+logout = 'assets/res-leftmenu/Log Out.png';
+
 }
