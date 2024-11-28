8d63c36e0f30fe97ab51d270636caac9eb947cb1
Thuy Duong <tttduong23@gmail.com>
2024-11-28 09:47:14 +0700

    gắn leftmenu vào setting page

--- angular_project_web/src/app/pages/setting-page/setting-page.component.ts
+++ angular_project_web/src/app/pages/setting-page/setting-page.component.ts
@@ -12,4 +12,15 @@ export class SettingPageComponent {
   avatarPath = 'assets/images/avatar.jpg';
   cameraPath = 'assets/images/camera.png';
   logoutPath = 'assets/images/Log Out.png';
+
+  // icon left menu
+show = 'assets/res-leftmenu/Arrow down-circle.png';
+home = 'assets/res-leftmenu/Squircle.png';
+sort = 'assets/res-leftmenu/Discover.png';
+recent = 'assets/res-leftmenu/Recent.png';  
+playlists = 'assets/res-leftmenu/Playlists.png';
+watchlist = 'assets/res-leftmenu/Watchlist.png';
+continue = 'assets/res-leftmenu/Continue.png';
+settings = 'assets/res-leftmenu/Settings.png';
+logout = 'assets/res-leftmenu/Log Out.png';
 }
