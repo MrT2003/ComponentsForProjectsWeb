65182746855fa24b1f7646398e8edd5a85fb9f85
MrT2008 <haitrieu6212@gmail.com>
2024-11-28 23:46:06 +0700

    fixing left menu

--- angular_project_web/src/app/pages/newest-film-page/newest-film-page.component.ts
+++ angular_project_web/src/app/pages/newest-film-page/newest-film-page.component.ts
@@ -1,24 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-newest-film-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './newest-film-page.component.html',
-  styleUrl: './newest-film-page.component.css'
+  styleUrl: './newest-film-page.component.css',
 })
 export class NewestFilmPageComponent {
   lokiPath = 'assets/images/loki.jpg';
+  sideBarPath = 'assets/res-leftmenu/sidebar.png';
 
-   // icon left menu
-show = 'assets/res-leftmenu/Arrow down-circle.png';
-home = 'assets/res-leftmenu/Squircle.png';
-sort = 'assets/res-leftmenu/Discover.png';
-recent = 'assets/res-leftmenu/Recent.png';  
-playlists = 'assets/res-leftmenu/Playlists.png';
-watchlist = 'assets/res-leftmenu/Watchlist.png';
-continue = 'assets/res-leftmenu/Continue.png';
-settings = 'assets/res-leftmenu/Settings.png';
-logout = 'assets/res-leftmenu/Log Out.png';
+  // icon left menu
+  show = 'assets/res-leftmenu/Arrow down-circle.png';
+  home = 'assets/res-leftmenu/Squircle.png';
+  sort = 'assets/res-leftmenu/Discover.png';
+  recent = 'assets/res-leftmenu/Recent.png';
+  playlists = 'assets/res-leftmenu/Playlists.png';
+  watchlist = 'assets/res-leftmenu/Watchlist.png';
+  continue = 'assets/res-leftmenu/Continue.png';
+  settings = 'assets/res-leftmenu/Settings.png';
+  logout = 'assets/res-leftmenu/Log Out.png';
+
+  isCollapsed = false; // Trạng thái menu: mở (false) hoặc thu nhỏ (true)
+
+  toggleMenu(): void {
+    this.isCollapsed = !this.isCollapsed; // Đổi trạng thái
+  }
 }
