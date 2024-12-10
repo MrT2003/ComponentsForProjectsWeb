86464adc3fcc7596127c083618d05f5e08c9dcf7
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-12-11 01:29:07 +0700

    update responsive for description page

--- angular_project_web/src/app/pages/description-page/description-page.component.ts
+++ angular_project_web/src/app/pages/description-page/description-page.component.ts
@@ -7,6 +7,7 @@ import { LeftMenuComponent } from '../../components/left-menu/left-menu.componen
 import { FilmGridComponent } from '../../components/film-grid/film-grid.component';
 //SERVICES   
 import { MovieService } from '../../service/MovieService/movie.service';
+import { MenuToggleService } from '../../service/menu-toggle-service.service';
 //MODELS
 import { APIMoviesModel, MovieList, NewestList } from '../../model/Movies';
 
@@ -34,22 +35,20 @@ export class DescriptionPageComponent implements OnInit {
   logout = 'assets/res-leftmenu/Log Out.png';
   sideBarPath = 'assets/res-leftmenu/sidebar.png';
 
-  isCollapsed = false; // Trạng thái menu: mở (false) hoặc thu nhỏ (true)
+  isLeftMenuOpen = false; 
 
   description: any;
   routerDesc = inject(Router);
-  constructor(private router: ActivatedRoute) {}
+  constructor(private router: ActivatedRoute, private menuToggleService: MenuToggleService) {}
   movieService = inject(MovieService);
   newestList = signal<NewestList[]>([]);
 
   ngOnInit(): void {
     this.loadDescription()
     this.loadNewestMovies();
-  }
-
-
-  toggleMenu(): void {
-    this.isCollapsed = !this.isCollapsed; // Đổi trạng thái
+    this.menuToggleService.menuState$.subscribe((state) => {
+      this.isLeftMenuOpen = state;
+    })
   }
 
   loadDescription() {
