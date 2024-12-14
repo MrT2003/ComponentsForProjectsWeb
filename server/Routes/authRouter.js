09760a0968990cafe8def0702b32862e3bc3f7ba
MrT2008 <haitrieu6212@gmail.com>
2024-12-14 12:35:58 +0700

    done login by google

--- server/Routes/authRouter.js
+++ server/Routes/authRouter.js
@@ -1,31 +1,44 @@
 import express from "express";
 import passport from "passport";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 // Google Login Route
-router.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
 // Google Callback Route
 router.get(
   "/auth/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
-    const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-    res.json({ message: "Successfully logged in with Google!", user: req.user, token });
+    const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("jwt", token, { httpOnly: true, secure: false });
+    res.redirect("http://localhost:4200/home");
   }
 );
 
 // Facebook Login Route
-router.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email"] }));
+router.get(
+  "/auth/facebook",
+  passport.authenticate("facebook", { scope: ["email"] })
+);
 
 // Facebook Callback Route
 router.get(
   "/auth/facebook/callback",
   passport.authenticate("facebook", { failureRedirect: "/" }),
   (req, res) => {
-    res.json({ message: "Successfully logged in with Facebook!", user: req.user });
+    res.json({
+      message: "Successfully logged in with Facebook!",
+      user: req.user,
+    });
   }
 );
 
